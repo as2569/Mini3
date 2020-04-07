ee46fe1b80081d1140a7e946687c3a23abb8683c
Adam <as2569@njit.edu>
2020-04-06 23:39:44 -0400

    interface and ajax

--- app/forms.py
+++ app/forms.py
@@ -1,6 +1,7 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, BooleanField, SubmitField
-from wtforms.validators import DataRequired
+from wtforms.validators import ValidationError, DataRequired
+from app.models import User
 
 class LoginForm(FlaskForm):
 	username = StringField('Username', validators=[DataRequired()])
@@ -8,4 +9,13 @@ class LoginForm(FlaskForm):
 	remember_me = BooleanField('Remember Me')
 	submit = SubmitField('Sign In')
 
+class RegistrationForm(FlaskForm):
+	username = StringField('Username', validators=[DataRequired()])
+	password = PasswordField('Password', validators=[DataRequired()])
+	submit = SubmitField('Register')
+
+	def validate_username(self, username):
+		user = User.query.filter_by(username=username.data).first()
+		if user is not None:
+			raise ValidationError("Please use different username")
 
--- app/routes.py
+++ app/routes.py
@@ -1,14 +1,27 @@
-from flask import render_template, flash, redirect, url_for
-from app import app
-from app.forms import LoginForm
+from flask import render_template, flash, redirect, url_for, request
+from app import app, db
+from app.forms import LoginForm, RegistrationForm
 from flask_login import current_user, login_user, logout_user, login_required
 from app.models import User
+from werkzeug.urls import url_parse
+from flask import request
 
 @app.route('/')
 @app.route('/index')
 @login_required
 def index():
-	return render_template("index.html", title='Home Page')
+	return render_template('index.html', title='Home Page')
+
+@app.route('/calculator')
+def calculator():
+	return render_template('calculator.html')
+
+@app.route('/update', methods=['GET', 'POST'])
+def update():
+	buttonValue = request.form['data']
+	result = request.form['result'] + buttonValue
+	print("returning " + result)
+	return render_template('calculator.html', result=result)
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
@@ -23,7 +36,7 @@ def login():
 		login_user(user, remember=form.remember_me.data)
 		next_page = request.args.get('next')
 		if not next_page or url_parse(next_page).netloc != '':
-			next_page = url_parse('index')
+			next_page = url_for('index')
 		return redirect(url_for(next_page))
 	return render_template('login.html', title='Sign In', form=form)
 
@@ -31,3 +44,17 @@ def login():
 def logout():
 	logout_user()
 	return redirect(url_for('index'))
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+	if current_user.is_authenticated:
+		return redirect(url_for('index'))
+	form = RegistrationForm()
+	if form.validate_on_submit():
+		user = User(username=form.username.data)
+		user.set_password(form.password.data)
+		db.session.add(user)
+		db.session.commit()
+		flash('You are registered')
+		return redirect(url_for('login'))
+	return render_template('register.html', title='Register', form=form)
